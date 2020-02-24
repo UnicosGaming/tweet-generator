29a42dc862168c28eaef7354a5c289718b3f2448
Xavi <xavierserrate1@gamil.com>
2020-02-24 23:28:28 +0100

    New buttons for change logos and leagues

--- src/viewmodels/generator_viewmodel.py
+++ src/viewmodels/generator_viewmodel.py
@@ -4,16 +4,41 @@ from PyQt5 import QtCore
 from PyQt5.QtCore import pyqtSignal
 
 class GeneratorViewModel(QtCore.QObject):
-    onNexBackground = pyqtSignal(str)
+    onNextBackground = pyqtSignal(str)
+    onNextLogo_1 = pyqtSignal(str)
+    onNextLogo_2 = pyqtSignal(str)
+    onNextLeague = pyqtSignal(str)
 
     def __init__(self):
         super().__init__()
         
         self.backgrounds_path = os.path.join(os.getcwd(), "resources", "backgrounds")
+        self.Logo_1_path = os.path.join(os.getcwd(), "resources", "logos")
+        self.Logo_2_path = os.path.join(os.getcwd(), "resources", "logos")
+        self.League_path = os.path.join(os.getcwd(), "resources", "leagues")
 
         self.background_index = 0
+        self.Logo_1_index = 0
+        self.Logo_2_index = 0
+        self.League_index = 0
+        
         self.background_images = self.load_backgrounds()
+        self.Logo_1_images = self.load_logo_1()
+        self.Logo_2_images = self.load_logo_2()
+        self.leagues_images = self.load_leagues()
     
+
+
+
+######################################################################################################
+
+#############################          DEF LOADS               #######################################
+
+######################################################################################################
+
+
+
+
     def load_backgrounds(self):
         paths = []
         for d, _, f in os.walk(self.backgrounds_path):
@@ -22,12 +47,74 @@ class GeneratorViewModel(QtCore.QObject):
 
         return paths
 
+    def load_logo_1(self):
+        paths = []
+        for d, _, f in os.walk(self.Logo_1_path):
+            for file in f:
+                paths.append(os.path.join(d,file))
+
+        return paths
+
+    def load_logo_2(self):
+        paths = []
+        for d, _, f in os.walk(self.Logo_2_path):
+            for file in f:
+                paths.append(os.path.join(d,file))
+
+        return paths
+
+    def load_leagues(self):
+        paths = []
+        for d, _, f in os.walk(self.League_path):
+            for file in f:
+                paths.append(os.path.join(d,file))
+
+        return paths
+
+
+######################################################################################################
+
+#############################          NEXT PICTURES           #######################################
+
+######################################################################################################
+
+
     def next_background(self):
         self.background_index += 1
         
         if self.background_index >= len(self.background_images):
             self.background_index = 0
 
-        self.onNexBackground.emit(self.background_images[self.background_index])
+        self.onNextBackground.emit(self.background_images[self.background_index])
+
+
+    def next_logo_1(self):
+        self.Logo_1_index += 1
+        
+        if self.Logo_1_index >= len(self.Logo_1_images):
+            self.Logo_1_index = 0
+
+        self.onNextLogo_1.emit(self.Logo_1_images[self.Logo_1_index])
+
+
+    def next_logo_2(self):
+        self.Logo_2_index += 1
+        
+        if self.Logo_2_index >= len(self.Logo_2_images):
+            self.Logo_2_index = 0
+
+        self.onNextLogo_2.emit(self.Logo_2_images[self.Logo_2_index])
+
+
+    def next_league(self):
+        self.League_index += 1
+        
+        if self.League_index >= len(self.leagues_images):
+            self.League_index = 0
+
+        self.onNextLeague.emit(self.leagues_images[self.League_index])
         
 
+
+
+######################################################################################################
--- src/views/generator_view.py
+++ src/views/generator_view.py
@@ -8,21 +8,73 @@ class GeneratorView(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self, gui, viewmodel, parent=None):
         super().__init__(parent)
 
-        print("hola")
+       
         self.viewmodel = viewmodel
         self.setupUi(self)
 
         self.configure_signals()
 
+
+
+
+
     def configure_signals(self):
         # GUI signals
-        self.btnNext.clicked.connect(self.viewmodel.next_background)
+            #Backgrounds 
+        self.btnNext_Background.clicked.connect(self.viewmodel.next_background)
+            #Logos
+        self.btnNext_Logo.clicked.connect(self.viewmodel.next_logo_1)
+        self.btnNext_Logo_2.clicked.connect(self.viewmodel.next_logo_2)
+            #Leagues
+        self.btnNext_League.clicked.connect(self.viewmodel.next_league)
+
+
 
         # ViewModel signals
-        self.viewmodel.onNexBackground.connect(self.show_background)
+            #Backgrounds             
+        self.viewmodel.onNextBackground.connect(self.show_background)
+            #Logos
+        self.viewmodel.onNextLogo_1.connect(self.show_logo_1)
+        self.viewmodel.onNextLogo_2.connect(self.show_logo_2)
+            #Leagues
+        self.viewmodel.onNextLeague.connect(self.show_leagues)
         
+
+
+######################################################################################################
+
+#############################         SHOW PICTURES            #######################################
+
+######################################################################################################
+
+
     def show_background(self, image_path):
         image = QtGui.QImage(image_path)
         pixmap = QtGui.QPixmap.fromImage(image)
 
-        self.lbImage.setPixmap(pixmap.scaled(self.lbImage.size()))
+        self.lbl_background.setPixmap(pixmap.scaled(self.lbl_background.size()))
+
+
+
+    def show_logo_1(self, image_path):
+        image = QtGui.QImage(image_path)
+        pixmap = QtGui.QPixmap.fromImage(image)
+
+        self.lbl_logo_1.setPixmap(pixmap.scaled(self.lbl_logo_1.size()))
+
+
+
+    def show_logo_2(self, image_path):
+        image = QtGui.QImage(image_path)
+        pixmap = QtGui.QPixmap.fromImage(image)
+
+        self.lbl_logo_2.setPixmap(pixmap.scaled(self.lbl_logo_2.size()))
+
+
+
+    def show_leagues(self, image_path):
+        image = QtGui.QImage(image_path)
+        pixmap = QtGui.QPixmap.fromImage(image)
+
+        self.lbl_league.setPixmap(pixmap.scaled(self.lbl_league.size()))
+
